b3f9107ffe51cbd6f145a726922b59f516bc3f03
IriemDaLuz <yenduuca2005@gmail.com>
2024-11-21 08:36:42 +0000

    Task.kt:Definimos la entidad TiposTareas y actualizamos Task para incluir la clave foránea (id_tipostareas)

--- app/src/main/java/com/example/composeroomapp/data/Task.kt
+++ app/src/main/java/com/example/composeroomapp/data/Task.kt
@@ -1,10 +1,23 @@
 package com.example.composeroomapp.data
+
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "tasks")
+@Entity(
+    tableName = "tasks",
+    foreignKeys = [
+        ForeignKey(
+            entity = TaskType::class,
+            parentColumns = ["id"],
+            childColumns = ["id_tipostareas"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Task(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
-    val isCompleted: Boolean = false
+    val isCompleted: Boolean = false,
+    val id_tipostareas: Int // Relación con TiposTareas
 )
