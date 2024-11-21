eee823c975019beda78fefbb6cd1f376a5b9d8b9
IriemDaLuz <yenduuca2005@gmail.com>
2024-11-21 08:37:55 +0000

    (Nueva)TaskType.kt: Creamos La tabla para los tipose de tareas

---
+++ app/src/main/java/com/example/composeroomapp/data/TaskType.kt
@@ -0,0 +1,10 @@
+package com.example.composeroomapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task_types")
+data class TaskType(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String
+)
