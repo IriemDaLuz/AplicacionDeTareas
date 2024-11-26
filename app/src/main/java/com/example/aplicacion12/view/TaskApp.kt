f785c0d2972438c2645f9470b466c403a13078fc
IriemDaLuz <yenduuca2005@gmail.com>
2024-11-26 13:14:07 +0000

    funciones crud de tasktype implementadas en Mostrar lista tarea

--- app/src/main/java/com/example/aplicacion12/view/TaskApp.kt
+++ app/src/main/java/com/example/aplicacion12/view/TaskApp.kt
@@ -2,6 +2,7 @@ package com.example.aplicacion12.view
 
 import android.util.Log
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -12,6 +13,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,7 +47,7 @@ fun TaskApp(viewModel: TaskViewModel) {
             text = "Gestor de Tareas",
             fontWeight = FontWeight.Bold,
             fontSize = 18.sp,
-            modifier = Modifier.padding(top = 18.dp, bottom = 46.dp)
+            modifier = Modifier.padding(top = 18.dp, bottom = 24.dp)
         )
 
         // Crear o seleccionar tipo de tarea
@@ -141,7 +143,20 @@ fun TaskApp(viewModel: TaskViewModel) {
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 items(taskTypes) { taskType ->
-                    Text(taskType.title) // Muestra el título de cada tipo de tarea
+                    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+                        Text(taskType.title)
+                        // Muestra el título de cada tipo de tarea
+                        Row (modifier = Modifier.padding(end = 10.dp)){
+                            Button(onClick = {viewModel.updateTaskType(taskType)}) {
+
+                                Text("Editar")
+                            }
+                            Button(onClick = {viewModel.deleteTaskType(taskType)}) {
+                                Text(text = "Eliminar")
+                            }
+                        }
+                    }
+
                 }
             }
         }
